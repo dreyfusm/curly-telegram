1cc217ff453566fb3e6fdff52014bb2a3c17f00d
Mike Dreyfus <mdreyfus@urbn.com>
2016-12-20 23:08:33 -0500

    Added more for the battle ship application

--- python/Battleship.py
+++ python/Battleship.py
@@ -18,6 +18,23 @@ def rand_col(board):
     rand_col = randint(0,len(board) - 1)
     return rand_col
 
-ship_row = rand_row(board)
+#Generates random coordinates
+ship_row = int(rand_row(board))
+ship_col = int(rand_col(board))
 
-ship_col =  rand_col(board)
+#Ask user to guess
+print "ship row " + str(ship_row)
+print "ship row " + str(ship_col)
+
+guess_row = int(raw_input("Guess Row:"))
+guess_col = int(raw_input("Guess Col:"))
+
+if guess_row == ship_row and guess_col == ship_col:
+    print "It's a hit"
+    board[ship_row][ship_col] = "X"
+    print_board(board)
+
+else:
+    print "Miss"
+    board[guess_row][guess_col] = "*"
+    print_board(board)
