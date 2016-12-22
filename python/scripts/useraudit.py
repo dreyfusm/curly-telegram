eeeb21c54d76a23ea17b58f00cb260a9a0aa9343
Mike Dreyfus <mdreyfus@urbn.com>
2016-12-22 14:16:06 -0500

    Checks and prints who has sudo access

--- python/scripts/useraudit.py
+++ python/scripts/useraudit.py
@@ -7,20 +7,46 @@ env.hosts = []
 list_common =[]
 
 #Prints users who have access to the system
-def printusers ():
+def printusers():
     output['everything'] = False
     host = sudo("hostname")
     owner = (sudo(str("ls -l /home | awk '{print $3}'")))
     directory = (sudo(str("ls -l /home | awk '{print $9}'")))
 
+#parses users into lists to be compared
     newowner = owner.splitlines()
     newdirectory = directory.splitlines()
 
-    list_common = [host]
+#compares the lists. If the owner == the directory name then they are on the list
+#logic here is that if the owner is root then they have been revoked from the System
+#via puppet (user module will change ownership to root)
+    list_common = []
     for a, b in zip(newowner, newdirectory):
         if a == b:
             list_common.append(a)
-    print list_common
+    print ""
+    print "Users with access to " + host
+    print ', '.join(list_common)
+    print ""
+#now check to see who has root access on System
+#since we have the users on the system, we will
+#grab the users in the wheel group
+
+    sudolist = sudo('cat /etc/group| grep wheel |cut -d: -f4')
+
+    #print "debug after sudo list"
+    #print sudolist
+
+
+    sortlist = []
+    for user in list_common:
+        if user in sudolist:
+            sortlist.append(user)
+    print "Here are the current active users with sudo access:"
+    print ""
+    print ', '.join(sortlist)
+    print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
 
 
 
