82f6e2a696234700802ba5fac6e28515872d35b9
Mike Dreyfus <mdreyfus@urbn.com>
2016-12-22 14:56:04 -0500

    Script exports to CSV

--- python/scripts/useraudit.py
+++ python/scripts/useraudit.py
@@ -1,15 +1,31 @@
-#purpose of script. Grab the output of
+# Mike Dreyfus
+#purpose of script. Grab the output of /etc/home, print users who are still active
+# and of those active users, print who has sudo access
 from fabric.api import env, run, sudo, hide, settings, output
+import csv
 
 env.user = ''
 env.password = ''
 env.hosts = []
-list_common =[]
+
+#declaring global variables
+#one for the csvlist that we will export
+#turn keeps track of how many times we've looped through
+csvlist = []
+turn = 0
+hostnum = len(env.hosts)
+title1 = ["Users below currently have access to the system"]
+title2 = ["Users below currently have access to the system"]
 
 #Prints users who have access to the system
 def printusers():
+    global turn
+    global csvlist
+    global hostnum
+    global title1
+    global title2
     output['everything'] = False
-    host = sudo("hostname")
+    host = [sudo("hostname").upper()]
     owner = (sudo(str("ls -l /home | awk '{print $3}'")))
     directory = (sudo(str("ls -l /home | awk '{print $9}'")))
 
@@ -21,11 +37,11 @@ def printusers():
 #logic here is that if the owner is root then they have been revoked from the System
 #via puppet (user module will change ownership to root)
     list_common = []
-    for a, b in zip(newowner, newdirectory):
-        if a == b:
-            list_common.append(a)
+    for name1, name2 in zip(newowner, newdirectory):
+        if name1 == name2:
+            list_common.append(name1)
     print ""
-    print "Users with access to " + host
+    print "Users with access to " + host[0]
     print ', '.join(list_common)
     print ""
 #now check to see who has root access on System
@@ -47,6 +63,23 @@ def printusers():
     print ', '.join(sortlist)
     print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
+    #Lastly, add the hostname, user list, and sudo list to the master CSV list
+    csvlist.append(host)
+    csvlist.append(title1)
+    csvlist.append(list_common)
+    csvlist.append(title2)
+    csvlist.append(sortlist)
+
+    turn += 1
+    print turn
+    #ones the number of turns is equal to the length of hosts in the list we will create the CSV
+    if turn == hostnum:
+        print "Exporting all to CSV"
+        res = csvlist
+        csvfile = "useradit.csv"
+        with open(csvfile, "w") as writeout:
+            writer = csv.writer(writeout, lineterminator='\n')
+            writer.writerows(res)
 
 
 
