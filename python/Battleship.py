5b2da887101f9a89d6aed994d590b68641825932
Mike Dreyfus <mdreyfus@urbn.com>
2016-12-21 20:22:59 -0500

    Turned validation into functions

--- python/Battleship.py
+++ python/Battleship.py
@@ -1,5 +1,6 @@
 #things to add
 ##-second ship
+##- two player
 ## -ships bigger than 1 space
 ## -more turns.. bigger board?..
 ## -let user pick amount of ships
@@ -29,6 +30,18 @@ def rand_col(board2):
     rand_column = randint(1, len(board2) - 1)
     return rand_column
 
+def guess_validate(num):
+    if num < 1 or num > 5:
+        return False
+    else:
+        return True
+
+def duplicate_validate(row, col):
+    if board[row - 1][col - 1] == "*":
+        return False
+    else:
+        return True
+
 print ""
 print ""
 print "Welcome to Battle Ship. If you can guess where my ship is in four turns, you win!"
@@ -57,7 +70,7 @@ while play_again == 'yes':
                 while guess_row < 1 or guess_row > 5:
                     try:
                         guess_row = int(raw_input("Guess Row:")) #get input
-                        if guess_row < 1 or guess_row > 5: #check to make sure it's in range
+                        if guess_validate(guess_row) is False: #check to make sure it's in range
                             print "Invalid input"
                         else:
                             break
@@ -68,13 +81,13 @@ while play_again == 'yes':
                     try:
                         guess_col = int(raw_input("Guess Col:"))
                         #guess_col = 3
-                        if guess_col < 1 or guess_col > 5:
+                        if guess_validate(guess_col) is False:
                             print "Invalid input"
                         else:
                             break
                     except ValueError:
                         print "Input not valid"
-                if board[guess_row - 1][guess_col - 1] == "*": #check to make sure it's not already guessed
+                if duplicate_validate(guess_row, guess_col) is False: #check to make sure it's not already guessed
                     print "You've already guessed this coordinate!"
                 else:
                     break
@@ -94,14 +107,7 @@ while play_again == 'yes':
             if turn != 3:
                 print "Miss!"
                 print "~~~~~~~~~~~~~~"
-                if guess_col == 0:
-                    board[guess_row - 1][guess_col] = "*"
-                elif guess_row == 0:
-                    board[guess_row][guess_col - 1] = "*"
-                elif guess_col == 0 and guess_row == 0:
-                    board[guess_row][guess_col] = "*"
-                else:
-                    board[guess_row - 1][guess_col - 1] = "*"
+                board[guess_row - 1][guess_col - 1] = "*"
                 print_board(board)
                 print " this is turn", turn
                 print "~~~~~~~~~~~~~~ end of continue"
@@ -109,14 +115,7 @@ while play_again == 'yes':
                 print "~~~~~~~~~~~~~~ start of end else"
                 print "Game over"
                 print "My ship was at row %s, column %s!" % (ship1_col, ship1_row)
-                if ship1_col == 0:
-                    board[ship1_row - 1][ship1_col] = "X"
-                elif ship1_row == 0:
-                    board[ship1_row][ship1_col - 1] = "X"
-                elif ship1_col == 0 and ship1_row == 0:
-                    board[ship1_row][ship1_col] = "X"
-                else:
-                    board[ship1_row - 1][ship1_col - 1] = "X"
+                board[ship1_row - 1][ship1_col - 1] = "X"
                 print "~~~~~~~~~~~~~~"
                 print_board(board)
                 response = raw_input("Would you like to play again?")
