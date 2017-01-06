ad616bf4330b3b86efc40b3bae60b905dc671282
Mike Dreyfus <mdreyfus@urbn.com>
2017-01-06 14:21:08 -0500

    updated script to not use fabric

--- python/scripts/useraudit.py
+++ python/scripts/useraudit.py
@@ -1,85 +1,89 @@
 # Mike Dreyfus
 #purpose of script. Grab the output of /etc/home, print users who are still active
 # and of those active users, print who has sudo access
-from fabric.api import env, run, sudo, hide, settings, output
 import csv
+from pexpect import pxssh
 
-env.user = ''
-env.password = ''
-env.hosts = []
+master_access = {}  #uses list_common
+master_sudo = {}  #uses sortlist
+fail_list = []
 
-#declaring global variables
-csvlist = []
-turn = 0
-hostnum = len(env.hosts)
-title1 = ["Users below currently have access to the system"]
-title2 = ["Users below currently have access to the system"]
+try:
+    with open('', 'rb') as f:
+        temp_server_list = []
+        temp_server_list = [str(row) for row in csv.reader(f.read().splitlines())]
+    server_list = []
+    for stuff in temp_server_list:
+        new = stuff[2:-2]
+        server_list.append([new])
+except ValueError:
+    print "Error: Could not open CSV."
 
-#Prints users who have access to the system 
-#Note about fabric functions: They will iteriate through like a for loop and repeat
-#if you want to perserve variables you must make them global
-def printusers():
-    global turn
-    global csvlist
-    global hostnum
-    global title1
-    global title2
-    output['everything'] = False
-    host = [sudo("hostname").upper()]
-    owner = (sudo(str("ls -l /home | awk '{print $3}'")))
-    directory = (sudo(str("ls -l /home | awk '{print $9}'")))
+for i in server_list:
+    HOST = ' '.join(i).rstrip()
+    print "Trying: " + HOST
+    s = pxssh.pxssh()
+    try:
+        username = ""
+        password = ""
+        s.timeout = 5
+        s.login(HOST, username, password)
+        print "Login successful!"
+        s.sendline('ls -l /home|awk \'{print $3}\'')   # Grab owner
+        s.prompt()
+        owner = s.before
+        s.sendline('ls -l /home | awk \'{print $9}\'')  # Grab the directory name
+        s.prompt()
+        directory = s.before
+        s.sendline('cat /etc/group| grep wheel |cut -d: -f4')  #grab users in wheel group
+        s.prompt()
+        sudolist = s.before
+        newowner = owner.splitlines()  #parse lists to be sortable
+        newdirectory = directory.splitlines()
+        list_common = []  #Sorts both lists. If the owner name is in the directory name, the user exists
+        for name1, name2 in zip(newowner, newdirectory):
+            if name1 == name2:
+                list_common.append(name1)
+        sortlist = []  #Find out who has sudo access
+        for user in list_common:
+            if user in sudolist:
+                sortlist.append(user)
+        for n in list_common:                        #Adds users to dictionary as key value. If they are not in the dictionary, add them as a Key
+            if n not in master_access:               #After they are in the dictionary, check if they are in the list, and if so appened the host name to them
+                master_access[n] = []
+            for key, value in master_access.items():
+                if n == key:
+                    value.append(HOST)
 
-#formats strings so they can be compared
-    newowner = owner.splitlines()
-    newdirectory = directory.splitlines()
+        print str(HOST) + " done."
+        s.logout()  #End SSH session
+        s.close()   #Cleans object/garbage collects
+    except (AttributeError, TypeError, pxssh.ExceptionPxssh) as e:
+        print "Failed on " + HOST + "... Probably because SSH failed."
+        print e
+        fail_list.append(HOST)
 
-#compares the lists. If the owner == the directory name then they are on the list
-#logic here is that if the owner is root then they have been revoked from the System
-#via puppet (user module will change ownership to root)
-    list_common = []
-    for name1, name2 in zip(newowner, newdirectory):
-        if name1 == name2:
-            list_common.append(name1)
-    print ""
-    print "Users with access to " + host[0]
-    print ', '.join(list_common)
-    print ""
-#now check to see who has root access on System
-#since we have the users on the system, we will
-#grab the users in the wheel group
-
-    sudolist = sudo('cat /etc/group| grep wheel |cut -d: -f4')
-
-    #print "debug after sudo list"
-    #print sudolist
-
-
-    sortlist = []
-    for user in list_common:
-        if user in sudolist:
-            sortlist.append(user)
-    print "Here are the current active users with sudo access:"
-    print ""
-    print ', '.join(sortlist)
-    print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-
-    #Lastly, add the hostname, user list, and sudo list to the master CSV list
-    csvlist.append(host)
-    csvlist.append(title1)
-    csvlist.append(list_common)
-    csvlist.append(title2)
-    csvlist.append(sortlist)
-
-    turn += 1
-    print turn
-    #if turn == end of env.list then it prints the CSV
-    if turn == hostnum:
-        print "Exporting all to CSV"
-        res = csvlist
-        csvfile = "useradit.csv"
-        with open(csvfile, "w") as writeout:
-            writer = csv.writer(writeout, lineterminator='\n')
-            writer.writerows(res)
-
-
-
+master_access.pop('', 0)  #removes blank user
+print ""
+print "Exporting all to CSV"   #Begin CSV export
+master_fail = []
+master_fail.append(fail_list)  #Adds fail list to a list to be parsed correctly by csv writer
+res = master_access
+csvfile = "useradit.csv"
+with open('useraudit.csv', 'wb') as csv_file:
+    writer = csv.writer(csv_file)
+    for key, value in master_access.items():
+        writer.writerow([key] + master_access[key])
+    writer.writerow("")
+    writer.writerow(["I could not connect to the following hosts: "])
+    writer.writerow("")
+    for val in master_fail:
+        for i in val:
+            writer.writerow([i.rstrip()])
+        #writer.writerow(''.join(val))
+print ""
+print "Completed."
+print ""
+print "I could not connect to the following hosts (they're included in the spreadsheet): "
+for i in fail_list:
+    print ''.join(i).rstrip()
